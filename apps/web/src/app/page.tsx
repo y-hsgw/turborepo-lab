47015d4553610d84228352daaa711599ae69362f
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-07-15 23:38:00 +0900

    turbo.jsonの環境変数の検証

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1>{process.env.NEXT_PUBLIC_TITLE}</h1>
         <Image
           className={styles.logo}
           src="/next.svg"
@@ -14,38 +15,6 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
       <div>{add(1, 2)}</div>;
       <footer className={styles.footer}>

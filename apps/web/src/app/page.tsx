bd3ab2fa37ca78fc63ef3843f6860bf2d0786187
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-07-19 16:21:16 +0900

    webのe2e実装

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { add } from "@repo/math/add";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "トップページ",
+};
 
 export default function Home() {
   return (

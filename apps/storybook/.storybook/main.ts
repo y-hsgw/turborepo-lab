6de6075a4decbdb3e1e0097b67ea52b13efc4176
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-07-22 22:09:11 +0900

    appsにstorybookの実装

---
+++ apps/storybook/.storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+import { join, dirname } from "path";
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [],
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {},
+  },
+  staticDirs: ["../public"],
+};
+export default config;

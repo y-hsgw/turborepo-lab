fce8cbca70a68e2e0aee3b98939adc82e1560900
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-07-23 22:11:19 +0900

    lintの修正

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 import { join, dirname } from "path";
 
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value: string): string {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {

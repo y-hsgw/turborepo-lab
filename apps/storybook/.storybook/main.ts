6126359adb2870926f37ae653e86ef1631847d77
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-07-23 22:06:26 +0900

    パッケージを跨ぐStorybookを閲覧できるようにする

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -6,7 +6,16 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    {
+      directory: "../../../packages/ui/src",
+      titlePrefix: "ui",
+    },
+    {
+      directory: "../../web/src",
+      titlePrefix: "web",
+    },
+  ],
   addons: [],
   framework: {
     name: getAbsolutePath("@storybook/nextjs"),

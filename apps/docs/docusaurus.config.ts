c6cad8e6d8c275f3927704cbeea229ab37165918
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-07-27 14:27:39 +0900

    baseUrlの設定修正

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/turborepo-lab/",
 
   projectName: "docusaurus", // Usually your repo name.
 

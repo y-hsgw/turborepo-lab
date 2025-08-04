b5592fb67eb4df4212b50bd5191d328e721264fa
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-08-04 23:36:58 +0900

    Headerコンポーネント実装

---
+++ packages/ui/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/Button";
+
+export const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>My Application</h1>
+      <Button>ログイン</Button>
+    </header>
+  );
+};

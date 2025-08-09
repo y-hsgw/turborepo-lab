4099d06db4a568fa7f624d3a84231c666d77a836
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-08-09 15:06:04 +0900

    @repo/uiのstorybookでadditionalDataの適用

--- packages/ui/next.config.ts
+++ packages/ui/next.config.ts
@@ -1,8 +1,27 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  sassOptions: {
+    additionalData: (content: any, loaderContext: any) => {
+      const { resourcePath, rootContext } = loaderContext;
+      const absoluteCSSPath = path.join(
+        rootContext,
+        "src",
+        "styles",
+        "variables",
+        "size",
+      );
+      const relativePath = path.relative(
+        path.dirname(resourcePath),
+        absoluteCSSPath,
+      );
+
+      return `@use "${relativePath}";\n${content}`;
+    },
   },
   output: "standalone",
 };

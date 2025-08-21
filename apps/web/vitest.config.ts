23d2e26f88efdf1a545dc723884a0ad033e4e3e6
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-08-21 21:56:09 +0900

    vitest実行時にconfigLoaderのrunner指定

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import { additionalData } from "@repo/ui/configs/scss-additional-data";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: additionalData,
+      },
+    },
+  },
   test: {
     environment: "jsdom",
     css: true,

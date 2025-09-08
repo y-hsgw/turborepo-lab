3931dae034fbad77c08dabb068ddb68bf29122b6
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-09-08 22:39:53 +0900

    Headerにsvg適用とLayoutにHeader適用

--- packages/ui/src/components/Header.tsx
+++ packages/ui/src/components/Header.tsx
@@ -1,10 +1,15 @@
 import { Button } from "#components/Button";
+import Image from "next/image";
+import windowIcon from "#assets/window.svg?url";
+import Window from "#assets/window.svg";
 
 export const Header: React.FC = () => {
   return (
     <header>
       <h1>My Application</h1>
+      <Image src={windowIcon} width={20} height={20} alt="window" />
       <Button>ログイン</Button>
+      <Window width={20} />
     </header>
   );
 };

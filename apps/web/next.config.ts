d5d1fba15dd3dfae0bbf13641acae5d468bfb8f6
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-09-08 22:38:47 +0900

    next.configにsvgrの設定適用

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,9 +5,43 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
   output: "standalone",
   sassOptions: {
     additionalData: additionalData,
+  },
+  webpack: (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (
+        // @ts-expect-error
+        rule,
+      ) => rule.test?.test?.(".svg"),
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ["@svgr/webpack"],
+      },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
   },
 };
 

73df56fffe9d6e7ee9e1b2688189bfdc58d2172a
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-09-09 21:44:32 +0900

    storybookにsvgr適用

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -13,5 +13,31 @@ const config: StorybookConfig = {
       nextConfigPath: path.resolve(__dirname, "../next.config.ts"),
     },
   },
+  webpackFinal: (config) => {
+    const fileLoaderRule = config.module?.rules?.find(
+      (rule) =>
+        typeof rule === "object" &&
+        rule?.test instanceof RegExp &&
+        rule.test.test(".svg"),
+    );
+
+    if (typeof fileLoaderRule === "object" && fileLoaderRule !== null) {
+      config.module?.rules?.push(
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/,
+        },
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [/url/] },
+          use: ["@svgr/webpack"],
+        },
+      );
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+    return config;
+  },
 };
 export default config;
